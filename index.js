47ea0ff6ed2cc188634359503b10a5cdc37eaf00
Jonas Edson <130106426+jonaseedson@users.noreply.github.com>
2023-06-10 02:36:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,105 @@
+const image = document.getElementById('cover'),
+title = document.getElementById('music-title'),
+artist = document.getElementById('music-artist'),
+currentTimeEl = document.getElementById('current-time'),
+durationEl = document.getElementById('duration'),
+progress = document.getElementById('progress'),
+playerProgress = document.getElementById('player-progress'),
+prevBtn = document.getElementById('prev'),
+nextBtn = document.getElementById('next'),
+playBtn = document.getElementById('play'),
+background = document.getElementById('bg-img');
+
+const music = new Audio();
+
+const songs = [
+  {
+    path: 'assets/1.mp3',
+    displayName: 'Inochi no Tabetaka',
+    cover: 'assets/1.jpeg',
+    artist: 'Eve',
+  },
+  {
+    path: 'assets/2.mp3',
+    displayName: 'kaikaikitan',
+    cover: 'assets/2.webp',
+    artist: 'Eve',
+  },
+  {
+    path: 'assets/3.mp3',
+    displayName: 'Drop ',
+    cover: 'assets/3.jpeg',
+    artist: 'Connor Price & Zensery',
+  }, 
+  {
+    path: 'assets/4.mp3',
+    displayName: 'Until I Found You',
+    cover: 'assets/4.jpeg',
+    artist: 'Stephen Sanchez',
+  }
+];
+
+let musicIndex = 0;
+let isPlaying = false;
+
+function togglePlay(){
+  if(isPlaying){
+    pauseMusic();
+  }else {
+    playMusic();
+  }
+}
+
+function playMusic(){
+  isPlaying = true;
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Pause');
+  music.play();
+}
+
+function pauseMusic(){
+  isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
+  music.pause();
+}
+
+function loadMusic(song){
+  music.src = song.path;
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  image.src = song.cover;
+  background.src = song.cover;
+}
+
+function changeMusic(direction){
+  musicIndex = (musicIndex + direction + songs.length) % songs.length;
+  loadMusic(songs[musicIndex]);
+  playMusic();
+}
+
+function updateProgressBar() {
+  const { duration, currentTime } = music;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+
+  const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
+  durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
+  currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
+}
+
+function setProgressBar(e) {
+  const width = playerProgress.clientWidth;
+  const clickX = e.offsetX;
+  music.currentTime = (clickX / width) * music.duration;
+}
+
+playBtn.addEventListener ('click', togglePlay);
+prevBtn.addEventListener ('click', () => changeMusic(-1));
+nextBtn.addEventListener ('click', () => changeMusic(1));
+music.addEventListener('ended', () => changeMusic(1));
+music.addEventListener('timeupdate', updateProgressBar);
+playerProgress.addEventListener('click', setProgressBar);
+
+loadMusic(songs[musicIndex]);
+
